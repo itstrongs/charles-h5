00bcfc887b4d2c76951c139ab4a548d41c81c516
刘奉强 <liufengqiang@touchealth.com>
2020-04-15 10:40:04 +0800

    init

---
+++ src/components/test.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2 class="red">test....{{msg}}</h2>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        props: {
+            msg: {
+                type: String,
+                default: "sd"
+            }
+        },
+        methods: {
+
+        },
+        data() {
+
+        }
+    }
+</script>
+
+<style scoped>
+.red {
+    color: red;
+}
+</style>
