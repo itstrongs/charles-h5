eaf1f93a920dc3e88c119d239face468da4cd795
刘奉强 <liufengqiang@touchealth.com>
2020-05-23 17:55:01 +0800

    update

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // config 配置
 axios.defaults.timeout = 60 * 1000;
 // axios.defaults.baseURL = 'http://192.168.18.232:8090';
-axios.defaults.baseURL = 'http://129.204.231.152:8081';
+axios.defaults.baseURL = 'http://129.204.231.152:1781';
 
 // http request 拦截器
 axios.interceptors.request.use (
